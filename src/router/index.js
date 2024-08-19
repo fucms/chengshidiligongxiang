88b96276cec8f501c9b78a92b852eacab9a0ce11
jhlsss <2622310358@qq.com>
2024-08-19 14:10:24 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,106 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+/* Layout */
+import Layout from '@/layout'
+/* Router Modules */
+// import DocumentationRouter from "./modules/documentation"; //基础信息
+
+import AddressRouter from './lh_modlues/address'
+import Alh_resourceShowRouter from './lh_modlues/resourceShow'
+import Alh_devCenter from './lh_modlues/alh_devCenter'
+import Alh_devOps from './lh_modlues/alh_devOps'
+// import alh_CloudDataRouter from "./lh_modlues/cloudData";
+// import unifyRouter from "./lh_modlues/unify";
+
+// constantRoutes 没有权限的基页，所有角色都可以查看
+export const constantRoutes = [
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('@/views/redirect/index')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/auth-redirect',
+    component: () => import('@/views/login/auth-redirect'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/error-page/404'),
+    hidden: true
+  },
+  {
+    path: '/401',
+    component: () => import('@/views/error-page/401'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: '首页', icon: 'el-icon-s-home', affix: true }
+      }
+    ]
+  },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: '个人概要', icon: 'user', noCache: true }
+      }
+    ]
+  }
+]
+// asyncRoutes 需要根据用户角色动态加载的路由
+export const asyncRoutes = [
+  // DocumentationRouter,
+
+  AddressRouter,
+  Alh_resourceShowRouter,
+  Alh_devCenter,
+  Alh_devOps,
+  // alh_CloudDataRouter,
+  // unifyRouter,
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/router/lh_modlues/alh_devOps.js
@@ -0,0 +1,139 @@
+import Layout from "@/layout";
+
+const alh_devOpsRouter = {
+  path: "/alh_devOps",
+  component: Layout,
+  redirect: "/alh_devOps/alh_system/userList/index.vue",
+  name: "alh_devOps",
+  meta: {
+    title: "运维支撑",
+    icon: "nested",
+  },
+  children: [
+    {
+      path: "alh_system",
+      component: () => import("@/views/alh_devOps/alh_system/index.vue"), // Parent router-view
+      name: "alh_system",
+      meta: { title: "系统管理" },
+      redirect: "/alh_devOps/alh_system/userList/index.vue",
+      children: [
+        {
+          path: "userList",
+          component: () =>
+            import("@/views/alh_devOps/alh_system/userList/index.vue"),
+          name: "userList",
+          meta: { title: "用户管理" },
+        },
+        {
+          path: "role",
+          component: () =>
+            import("@/views/alh_devOps/alh_system/role/index.vue"),
+          name: "role",
+          meta: { title: "权限管理" },
+        },
+        {
+          path: "loglist",
+          component: () =>
+            import("@/views/alh_devOps/alh_system/loglist/index.vue"),
+          name: "loglist",
+          meta: { title: "日志管理" },
+        },
+        {
+          path: "table",
+          component: () =>
+            import("@/views/alh_devOps/alh_system/table/index.vue"),
+          name: "table",
+          meta: { title: "数据访问统计" },
+        },
+        {
+          path: "serve",
+          component: () =>
+            import("@/views/alh_devOps/alh_system/serve/index.vue"),
+          name: "serve",
+          meta: { title: "服务使用登记" },
+        },
+      ],
+    },
+    {
+      path: "watch",
+      component: () => import("@/views/alh_devOps/watch/index.vue"),
+      name: "watch",
+      meta: { title: "系统监控" },
+      redirect: "/alh_devOps/watch/watch_able/index.vue",
+      children: [
+        {
+          path: "watch_able",
+          component: () =>
+            import("@/views/alh_devOps/watch/watch_able/index.vue"),
+          name: "watch_able",
+          meta: { title: "系统监控" },
+        },
+        {
+          path: "watch_status",
+          component: () =>
+            import("@/views/alh_devOps/watch/watch_status/index.vue"),
+          name: "watch_status",
+          meta: { title: "系统状态监控" },
+        },
+        {
+          path: "tapList",
+          component: () =>
+            import("@/views/alh_devOps/watch/tapList/index.vue"),
+          name: "tapList",
+          meta: { title: "资源目录建设" },
+        },
+        {
+          path: "address_list",
+          component: () =>
+            import("@/views/alh_devOps/watch/address_list/index.vue"),
+          name: "address_list",
+          meta: { title: "地图图层管理" },
+        },
+        {
+          path: "address_name",
+          component: () =>
+            import("@/views/alh_devOps/watch/address_name/index.vue"),
+          name: "address_name",
+          meta: { title: "地名地址" },
+        },
+        {
+          path: "space_list",
+          component: () =>
+            import("@/views/alh_devOps/watch/space_list/index.vue"),
+          name: "space_list",
+          meta: { title: "空间数据管理" },
+        },
+        {
+          path: "space_status",
+          component: () =>
+            import("@/views/alh_devOps/watch/space_status/index.vue"),
+          name: "space_status",
+          meta: { title: "数据分类与管理标准化" },
+        },
+        {
+          path: "address_list",
+          component: () =>
+            import("@/views/alh_devOps/watch/address_id/index.vue"),
+          name: "address_list",
+          meta: { title: "地理编码管理" },
+        },
+        {
+          path: "watch_resource",
+          component: () =>
+            import("@/views/alh_devOps/watch/watch_resource/index.vue"),
+          name: "watch_resource",
+          meta: { title: "资源管理行为监控" },
+        },
+        {
+          path: "contents_proccess",
+          component: () =>
+            import("@/views/alh_devOps/watch/contents_proccess"),
+          name: "contents_proccess",
+          meta: { title: "资源内容审核" },
+        },
+      ],
+    },
+  ],
+};
+
+export default alh_devOpsRouter;
